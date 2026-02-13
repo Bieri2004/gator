bd2c187580069edd8c596370a2dd92badd59b237
Pascal Keller <jaures@gmx.de>
2026-02-13 07:24:54 +0100

    added

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -11,5 +11,9 @@ RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE name = $1;
 
--- name: ResetUsers :exec
+-- name: DeleteUsers :exec
 DELETE FROM users;
+
+-- name: GetUsers :many
+SELECT * FROM users;
+
